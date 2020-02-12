0138be1c6516725453073a18370080ad4b40d417
PeterKimG <myopinion@naver.com>
2020-02-12 14:42:22 +0900

    test

---
+++ models/Daycare.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+// schema
+var daycareSchema = mongoose.Schema({
+  check:{type:boolean},
+  name:{type:String, required:[true,'Title is required!']},
+  owner:{type:String, required:[true,'Body is required!']},
+  type:{type:String, required:[true,'Body is required!']},
+  address:{type:String, required:[true,'Body is required!']},
+  phone:{type:String, required:[true,'Body is required!']},
+  fax:{type:String},
+  city:{type:String, required:[true,'Body is required!']},
+  street:{type:String, required:[true,'Body is required!']},
+  max:{type:String, required:[true,'Body is required!']},
+  now:{type:String, required:[true,'Body is required!']},
+  contract:{type:String},
+  bitch:{type:boolean},
+  mine:{type:String},
+  msg:{type:String},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  createdAt:{type:Date, default:Date.now},
+  updatedAt:{type:Date},
+});
+
+// model & export
+var Daycare = mongoose.model('daycare', daycareSchema);
+module.exports = Daycare;
---
+++ models/Kinder.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+// schema
+var kinderSchema = mongoose.Schema({
+  check:{type:boolean},
+  name:{type:String, required:[true,'Title is required!']},
+  owner:{type:String, required:[true,'Body is required!']},
+  type:{type:String, required:[true,'Body is required!']},
+  address:{type:String, required:[true,'Body is required!']},
+  phone:{type:String, required:[true,'Body is required!']},
+  fax:{type:String},
+  city:{type:String, required:[true,'Body is required!']},
+  street:{type:String, required:[true,'Body is required!']},
+  max:{type:String, required:[true,'Body is required!']},
+  now:{type:String, required:[true,'Body is required!']},
+  contract:{type:String},
+  bitch:{type:boolean},
+  mine:{type:String},
+  msg:{type:String},
+  author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
+  createdAt:{type:Date, default:Date.now},
+  updatedAt:{type:Date},
+});
+
+// model & export
+var Kinder = mongoose.model('kinder', kinderSchema);
+module.exports = Kinder;
