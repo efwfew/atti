3fa406f8d8eabd4cbde802ac2cf79848115d226e
PeterKimG <myopinion@naver.com>
2020-07-12 23:33:48 +0900

    code prettier

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var flash = require('connect-flash');
-var session = require('express-session');
-var passport = require('./config/passport');
-var util = require('./util');
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var flash = require("connect-flash");
+var session = require("express-session");
+var passport = require("./config/passport");
+var util = require("./util");
 var app = express();
-var mongodb = "mongodb+srv://atti_event:djfudnsqlalfqjsgh@attipageforworker-r3gp8.mongodb.net/test?retryWrites=true&w=majority";
+var mongodb =
+  "mongodb+srv://atti_event:djfudnsqlalfqjsgh@attipageforworker-r3gp8.mongodb.net/test?retryWrites=true&w=majority";
 
 // DB setting
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.Schema.Types.Boolean.convertToFalse.add('');
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+mongoose.Schema.Types.Boolean.convertToFalse.add("");
 mongoose.connect(mongodb);
 var db = mongoose.connection;
-db.once('open', function(){
-  console.log('DB connected');
+db.once("open", function () {
+  console.log("DB connected");
 });
-db.on('error', function(err){
-  console.log('DB ERROR : ', err);
+db.on("error", function (err) {
+  console.log("DB ERROR : ", err);
 });
 
 // Other settings
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname+'/public'));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.use(flash());
-app.use(session({secret:'MySecret', resave:true, saveUninitialized:true}));
+app.use(session({ secret: "MySecret", resave: true, saveUninitialized: true }));
 
 // Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Custom Middlewares
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.currentUser = req.user;
   res.locals.session = req.session;
   if (req.user) {
-      req.user.username;
+    req.user.username;
   }
   next();
 });
 
 // Routes
-app.use('/', require('./routes/home'));
-app.use('/posts', util.getPostQueryString, require('./routes/posts'));
-app.use('/users', require('./routes/users'));
-app.use('/samples', util.getPostQueryString, require('./routes/samples'));
-app.use('/reports', util.getPostQueryString, require('./routes/reports'));
-app.use('/files', util.getPostQueryString, require('./routes/files'));
-app.use('/suwon', util.getPostQueryString, require('./routes/daycares'));
-app.use('/comments', util.getPostQueryString, require('./routes/comments'));
-app.use('/commentsr', util.getPostQueryString, require('./routes/commentsr'));
+app.use("/", require("./routes/home"));
+app.use("/posts", util.getPostQueryString, require("./routes/posts"));
+app.use("/users", require("./routes/users"));
+app.use("/samples", util.getPostQueryString, require("./routes/samples"));
+app.use("/reports", util.getPostQueryString, require("./routes/reports"));
+app.use("/files", util.getPostQueryString, require("./routes/files"));
+app.use("/suwon", util.getPostQueryString, require("./routes/daycares"));
+app.use("/comments", util.getPostQueryString, require("./routes/comments"));
+app.use("/commentsr", util.getPostQueryString, require("./routes/commentsr"));
 
 // Port setting
 var port = 3000;
-app.listen(port, function(){
-  console.log('server on! http://localhost:'+ port);
+app.listen(port, function () {
+  console.log("server on! http://localhost:" + port);
 });
